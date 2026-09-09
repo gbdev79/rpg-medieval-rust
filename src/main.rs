406715aa7e9d419b2e569e937f50bf97c436d7e5
Gabriel Gonçalves <ctt.gabrielggo@gmail.com>
2026-09-09 01:27:43 -0300

    Menu principal, nome, vocação, boas-vindas, cidade/bosque

---
+++ src/main.rs
@@ -0,0 +1,62 @@
+use std::io;
+use textwrap::fill;
+
+fn main() {
+    println!("=== RPG MEDIEVAL === \n");
+
+    println!("Menu Principal: \n 
+    1. Novo jogo
+    2. Carregar jogo
+    ");
+
+    let mut input = String::new();
+    let mut nome_personagem = String::new();
+    let mut vocacao = String::new();
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Falha");
+
+    if input.trim() == "1" {
+        println!("Qual é o nome do seu personagem?");
+        io::stdin()
+            .read_line(&mut nome_personagem)
+            .expect("Falha");
+
+        println!("Qual vocação você deseja ser?\n
+        1. Guerreiro
+        2. Paladino
+        3. Druida
+        4. Mago
+        ");
+        io::stdin()
+            .read_line(&mut vocacao)
+            .expect("Falha");
+    }
+
+    println!("Olá, {}!", nome_personagem.trim());
+    println!();
+
+    println!("{}", fill("Seja bem-vindo a Eldoryan, um reino ancestral onde a própria essência da magia pulsa através da terra e ecoa nos ventos.", 70));
+    println!();
+    println!("{}", fill("Você acaba de adentrar um império de magnitude incomparável, moldado por eras de glória e lendas vivas, onde torres colossais arranham os céus e ruínas místicas aguardam para sussurrar seus segredos.", 70));
+    println!();
+
+    println!("{}", fill("A sua esquerda está o caminho da cidade e a sua direita o bosque da floresta", 70));
+    println!();
+    println!("{}", fill("Para onde você quer ir? \n
+    1. Para a cidade \n
+    2. Para o bosque", 70));
+
+    input.clear();
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Falha");
+
+    if input.trim() == "1" {
+        println!("Você foi em direção à cidade...");
+    }
+    
+}
+
