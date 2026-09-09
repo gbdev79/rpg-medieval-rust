4a07fdf780c631b182659e4d7058e1c4cc0ca10b
Gabriel Gonçalves <ctt.gabrielggo@gmail.com>
2026-09-09 19:23:52 -0300

    alterações de texto

--- src/agir.rs
+++ src/agir.rs
@@ -4,13 +4,13 @@ pub fn atacar() {
 
     let dado = rolar_d6();
     if dado <= 2 {
-        println!("Você errou o ataque!")
+        println!("> Você errou o ataque!")
     } else if dado == 3 {
-        println!("Você acertou de raspão")
+        println!("> Você acertou de raspão")
     } else if dado == 4 || dado == 5 {
-        println!("Você acertou o ataque")
+        println!("> Você acertou o ataque")
     } else {
-        println!("Você acertou um ataque crítico")
+        println!("> Você acertou um ataque crítico")
     }
 
 }
