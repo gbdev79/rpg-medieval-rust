3758f41b770099df7165bb4f1c9740a0eb499eed
Gabriel Gonçalves <ctt.gabrielggo@gmail.com>
2026-09-09 02:42:01 -0300

    Inserida fn nova_historia() em novo_jogo.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use std::io;
-use textwrap::fill;
 
 mod novo_jogo;
 
@@ -11,8 +10,6 @@ fn main() {
     2. Carregar jogo
     ");
 
-
-
     let mut input = String::new();
 
     io::stdin()
@@ -21,35 +18,12 @@ fn main() {
 
     let (nome, vocacao) = if input.trim() == "1" {
         novo_jogo::criar_personagem()
-
     } else {
         println!("Não existe jogo salvo.");
         return;
     };
 
-    println!("Olá, {}... O grande {}!", nome, vocacao);
-    println!();
-
-    println!("{}", fill("Seja bem-vindo a Eldoryan, um reino ancestral onde a própria essência da magia pulsa através da terra e ecoa nos ventos.", 70));
-    println!();
-    println!("{}", fill("Você acaba de adentrar um império de magnitude incomparável, moldado por eras de glória e lendas vivas, onde torres colossais arranham os céus e ruínas místicas aguardam para sussurrar seus segredos.", 70));
-    println!();
-
-    println!("{}", fill("A sua esquerda está o caminho da cidade e a sua direita o bosque da floresta", 70));
-    println!();
-    println!("{}", fill("Para onde você quer ir? \n
-    1. Para a cidade \n
-    2. Para o bosque", 70));
-
-    input.clear();
-
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Falha");
-
-    if input.trim() == "1" {
-        println!("Você foi em direção à cidade...");
-    }
+    novo_jogo::nova_historia(&nome, &vocacao);
     
 }
 
--- src/novo_jogo.rs
+++ src/novo_jogo.rs
@@ -1,4 +1,5 @@
 use std::io;
+use textwrap::fill;
 
 pub fn criar_personagem() -> (String, String) {
 
@@ -40,4 +41,30 @@ pub fn criar_personagem() -> (String, String) {
         let vocacao_limpa = vocacao.trim().to_string();
 
         (nome_limpo, vocacao_limpa)
+}
+
+pub fn nova_historia(nome: &str, vocacao: &str) {
+    println!("Olá, {}... O grande {}!", nome, vocacao);
+    println!();
+
+    println!("{}", fill("Seja bem-vindo a Eldoryan, um reino ancestral onde a própria essência da magia pulsa através da terra e ecoa nos ventos.", 70));
+    println!();
+    println!("{}", fill("Você acaba de adentrar um império de magnitude incomparável, moldado por eras de glória e lendas vivas, onde torres colossais arranham os céus e ruínas místicas aguardam para sussurrar seus segredos.", 70));
+    println!();
+
+    println!("{}", fill("A sua esquerda está o caminho da cidade e a sua direita o bosque da floresta", 70));
+    println!();
+    println!("{}", fill("Para onde você quer ir? \n
+    1. Para a cidade \n
+    2. Para o bosque", 70));
+
+    let mut input = String::new();
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Falha");
+
+    if input.trim() == "1" {
+        println!("Você foi em direção à cidade...");
+    }
 }
